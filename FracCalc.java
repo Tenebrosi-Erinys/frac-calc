5769f3988f43ac37136872c9c0ed9e5ed37a7aef
Nick Erb <44888363+Tenebrosi-Erinys@users.noreply.github.com>
2018-11-16 09:17:19 -0800

    Update FracCalc.java

--- FracCalc.java
+++ FracCalc.java
@@ -2,19 +2,17 @@ import java.util.Scanner;
 public class FracCalc{
    public static Scanner userInput = new Scanner(System.in); 
    public static void main(String args[]){
-      //hasQuit is a boolean that returns true when "quit" is input
-      boolean hasQuit = false;
       //the loop stops when hasQuit returns true, so that it exits the loop and the program
-      while(!hasQuit){
+      while(true){
          System.out.print("Enter: ");
          String input = userInput.nextLine();
          Scanner parser = new Scanner(input);
          //normalizes input to allow for any combination of capitals and lowercase
          input = input.toLowerCase();
-         System.out.println(processCommand(input));
          if(input.startsWith("quit")){
-            hasQuit = true;
+            break;
          }
+         System.out.println(processCommand(input));
       }
    }
    public static String processCommand(String input){
@@ -24,11 +22,8 @@ public class FracCalc{
       else if(input.startsWith("help")){
          return processHelp(input);
       }
-      else if(!input.startsWith("quit")){
+      else {
          return input;
-      }
-      else{
-         return "Quit";
       }
    }
    public static String processHelp(String input){
